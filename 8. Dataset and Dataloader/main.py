8242b8e80eeb24c1bef0e9fb1d803a3052c7d4f3
yungchih0209 <chih105015@gmail.com>
2023-07-14 21:35:25 +0800

    New Dataset

--- 8. Dataset and Dataloader/main.py
+++ 8. Dataset and Dataloader/main.py
@@ -30,7 +30,7 @@ class WineDataset(Dataset):
     
     def __init__(self):
         # data loading
-        xy = np.loadtxt('./wine.csv',delimiter=",",dtype=np.float32 ,skiprows=1)
+        xy = np.loadtxt('../Data/wine/wine.csv',delimiter=",",dtype=np.float32 ,skiprows=1)
         self.x= torch.from_numpy(xy[:,1:])
         self.y =torch.from_numpy(xy[:,[0]]) # n_samples , 1
         self.n_samples =xy.shape[0]
